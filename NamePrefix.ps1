ccd6b7fbc9c51db37fad4333dfb07ffcafd1a653
Airdeca <89158879+Airdeca@users.noreply.github.com>
2021-11-30 20:12:35 +0100

    Get the typical Dutch prefixes out of a SurName
    Trying to get rid of 'tussenvoegsels'.

---
+++ NamePrefix.ps1
@@ -0,0 +1,19 @@
+﻿<#
+30-11-2021 Rob de Korte
+This script strips the prefix (tussenvoegsel) off a SurName and generates a UserName out of the 'short' SurName
+Doesn't work very well yet with double SurNames. (Freule van Voorst tot Voorst messes things up royally)
+#>
+
+# Input Variables
+$GivenName = "Freule"
+$SurName = "van Voorst tot Voorst"
+
+# Chop it up
+$SurArray = $SurName.Split(" ")
+$SurShort = $SurArray[-1]    # Assume that the last word in the name is the actual SurName (not always true)
+$Prefix = $SurName.Replace($SurShort,"")   # the prefix must be anything but the last word, then  (again, not always true)
+$DisplayName = "$GivenName $Prefix$SurShort"
+$UserLogonName = ("$SurShort$($GivenName.Substring(0,1))").ToLower()
+
+Write-Host "Display Name is: $DisplayName"
+Write-Host "User Name is: $UserLogonName"
